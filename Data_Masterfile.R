2500286b43541d980cb355f924c6e6338f86b9a6
evanczopek <evanbcz@gmail.com>
2017-01-29 20:25:38 -0500

    Inital import data cleaned.

--- Data_Masterfile.R
+++ Data_Masterfile.R
@@ -1,8 +1,30 @@
 # Evan Czopek - 1/29/2017 6:00pm
 
-
+# Query of Sports Reference to get relevant table on all stats for NCAA teams. 
 library(XML)
 url_one <- "http://www.sports-reference.com/cbb/seasons/2017-school-stats.html"
-all_data_df <- readHTMLTable(url_one,
-                         which = 1)
+cc = c(rep('character',2), rep('integer',3), rep('numeric',3), rep('integer',8), 'character', rep('integer',3),
+       'numeric', rep('integer',2), 'numeric', rep('integer',2), 'numeric', rep('integer',7))
+all_data_df <- readHTMLTable(url_one, which = 1, colClasses = cc )
+
+# Clears all rows that hold column headers. 
+del_rows = c(which(all_data_df$Rk == ''),which(all_data_df$Rk == 'Rk'))
+all_data_df = all_data_df[-del_rows,]
+
+# Clear redundant first column 'Rk'
+all_data_df$Rk = NULL
+all_data_df$`Â ` = NULL
+
+# Renaming columns to more clear entries for manipulation. See README for definitions.
+colnames(all_data_df) = c('school', 'gp', 'wins', 'loss', 'wlp', 'srs', 'sos', 'conf_wins',
+                          'conf_loss', 'home_wins', 'home_loss', 'away_wins', 'away_loss',
+                          'points_for', 'points_against', 'min_played', 'fg', 'fga', 'fgp', 
+                          'thrp', 'thrpa', 'thrpp', 'ft', 'fta', 'ftp', 'orb', 'trb', 'ast', 'stl', 
+                          'blk', 'tov', 'pf')
+
+# Adding new columns. Two pointers made, attempted and percentage.
+library(dplyr)
+all_data_df = mutate(all_data_df, twop = fg - thrp, twopa = fga - thrpa, twopp = twop / twopa)
+
+
 
--- README.md
+++ README.md
@@ -1,4 +1,41 @@
 # Madness-Analytics
 NCAA bracket analytics
 
-Initial query and cleaning of data to happen in Data_Masterfile.R
+Initial query and cleaning of data to happen in Data_Masterfile.R. 
+
+Data frame column names and definitions:
+'school' - school name
+'gp' - total games played
+'wins' - total wins
+'loss' - total losses
+'wlp' - win/loss percentage
+'srs' - 'simple rating system' from Sports Reference
+'sos' - 'strength of schedule' from Sports Reference
+'conf_wins' - conference wins
+'conf_loss' - conference losses
+'home_wins' - home wins
+'home_loss' - homw losses
+'away_wins'  - away wins
+'away_loss' - away losses
+'points_for' - total points scored for
+'points_against' - total points scored against
+'min_played' - total minutes played 
+'fg' - total field goals made
+'fga' - total field goal attempts
+'fgp' - total field goal percentage
+'thrp' - total 3 pointers made
+'thrpa' - total 3 pointers attempted
+'thrpp' - total 3 pointers percentage
+'ft' - total free throws made
+'fta' - total free throws attempted
+'ftp' - total free throw percentage
+'orb' - total offensive rebounds
+'trb' - total rebounds
+'ast' - total assists
+'stl' - total steals 
+'blk' - total blocks
+'tov' - total turnovers
+'pf' - total personal fouls committed 
+'twop' - two points shots made
+'twopa' - two point shots attempted
+'twopp' - two point shot percentage made
